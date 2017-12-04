8a0b938729d5db4d24fcc9321b123c8d98261ae3
Jared <janderson1022@gmail.com>
2017-12-03 21:31:48 -0600

    Changed theme to bootstrap-darkly and added Books/Random view.

--- LibraryApp/App_Start/BundleConfig.cs
+++ LibraryApp/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace LibraryApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-darkly.css",
                       "~/Content/site.css"));
         }
     }
---
+++ LibraryApp/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryApp.Models;
+
+namespace LibraryApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "Cat's Cradle" };
+            return View(book);
+        }
+    }
+}
