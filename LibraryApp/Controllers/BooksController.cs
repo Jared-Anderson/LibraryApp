8fcaae5a318d623413e91de4bf10c05b90869bc0
Jared <janderson1022@gmail.com>
2017-12-03 23:36:07 -0600

    Added attribute routing to ByReleaseYear action.

--- LibraryApp/Controllers/BooksController.cs
+++ LibraryApp/Controllers/BooksController.cs
@@ -14,6 +14,31 @@ namespace LibraryApp.Controllers
         {
             var book = new Book() { Name = "Cat's Cradle" };
             return View(book);
+        
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy)) {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("books/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
